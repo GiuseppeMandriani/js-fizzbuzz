f546b983be27b1b778ccf99638b1b47eb178aa2e
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-03-23 17:36:09 +0100

    complete

--- js/script.js
+++ js/script.js
@@ -11,11 +11,12 @@
 
 
 for (var i = 1; i <= 100; i++) {
-    
+
+    // Prima Condizione più complessa
     if ((i % 3 == 0) && ( i % 5 == 0)){
         console.log('FizzBuzz')
     } else {
-
+        // Seconda Condizione 
         if (i % 3 == 0){
             console.log('Fizz');
         } else if (i % 5 == 0){
