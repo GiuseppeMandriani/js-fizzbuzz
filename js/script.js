55684b8ccc6b0ddf445fbd2a24a774a60db53c52
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-03-24 09:59:35 +0100

    metodo 1

--- js/script.js
+++ js/script.js
@@ -12,19 +12,32 @@
 
 for (var i = 1; i <= 100; i++) {
 
-    // Prima Condizione più complessa
-    if ((i % 3 == 0) && ( i % 5 == 0)){
-        console.log('FizzBuzz')
-    } else {
-        // Seconda Condizione 
-        if (i % 3 == 0){
-            console.log('Fizz');
-        } else if (i % 5 == 0){
-            console.log('Buzz');
-        } else console.log(i);
-    } 
-    
+    // Metodo 2
 
+    //Prima Condizione più complessa
+//     if ((i % 3 === 0) && ( i % 5 === 0)){
+//         console.log('FizzBuzz')
+//     } else {
+//         // Seconda Condizione 
+//         if (i % 3 === 0){
+//             console.log('Fizz');
+//         } else if (i % 5 === 0){
+//             console.log('Buzz');
+//         } else console.log(i);
+//     } 
+// }
+
+    // Metodo 1
+
+    if ((i % 3 === 0) && ( i % 5 === 0)){
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0){
+        console.log('Fizz');
+    } else if (i % 5 === 0){
+        console.log('Buzz');
+    } else console.log(i);
 }
+
+
 
 
